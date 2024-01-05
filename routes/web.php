042fb2c6f082db1347045b3d8f60613bc76c7416
ryannthegr8 <mmbaleryan@gmail.com>
2024-01-05 23:57:18 +0300

    functioning foreach

---
+++ routes/web.php
@@ -0,0 +1,25 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ToDoListController;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+// Default route is the one below
+
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+
+Route::get("/", [ToDoListController::class, 'index'] );
+
+Route::post('/saveItem', [ToDoListController::class, 'saveItem'])-> name('saveItem');
